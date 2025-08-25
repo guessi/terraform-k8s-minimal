4acbe21747766193ddabe6ba5ae9cf451eee7f4f
guessi <guessi@gmail.com>
2025-08-25 12:39:14 +0800

    Bump Terraform 1.12+ and dependencies

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Minimal setup for creating Kubernetes Resources
 ### Prerequisites
 
 - Proper configured `~/.kube/config`
-- Terraform >= 1.10
+- Terraform >= 1.12
 
 ### Setup Guide
 
--- versions.tf
+++ versions.tf
@@ -1,12 +1,12 @@
 terraform {
   # https://support.hashicorp.com/hc/en-us/articles/360021185113-Support-Period-and-End-of-Life-EOL-Policy
   # https://endoflife.date/terraform
-  required_version = ">= 1.10.5"
+  required_version = ">= 1.12.0"
 
   required_providers {
     # https://registry.terraform.io/providers/hashicorp/kubernetes/latest
     kubernetes = {
-      version = "~> 2.36"
+      version = "~> 2.38"
     }
   }
 }
